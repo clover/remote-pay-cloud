c6ac8fef36e3b9d13a0395d38cd5e314f6d9a233
mike hampton <mike@clover.com>
2016-07-08 13:27:16 -0600

    Make postversion.sh script commit files changed

--- scripts/postversion.sh
+++ scripts/postversion.sh
@@ -2,3 +2,4 @@
 echo $1
 sed -i -e 's/CLOVER_CLOUD_SDK_VERSION.*\"/CLOVER_CLOUD_SDK_VERSION = \"'$1'\"/g' Clover.js
 sed -i -e 's/CLOVER_CLOUD_SDK_VERSION.*\"/CLOVER_CLOUD_SDK_VERSION = \"'$1'\"/g' CloverConnectorImpl.js
+git add -A && git commit -m "$1"
