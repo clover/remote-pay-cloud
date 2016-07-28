7a8e10916884484ae72e81cbf43711fa31e18224
mike hampton <mike@clover.com>
2016-07-28 16:15:25 -0600

    Add git docs

--- scripts/generateDocs.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-echo "************************"
-echo "Generating documentation"
-echo "************************"
-./node_modules/.bin/jsdoc --readme ./README.md -c ./scripts/conf.json -d docs
-echo "************************"
-echo "Documentation generated, see " "file://"$(pwd)"/docs/index.html"
-echo "************************"
