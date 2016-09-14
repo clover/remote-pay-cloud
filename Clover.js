0d248bf2393f647319dbee5b6679c62d92542b7f
mike hampton <mike@clover.com>
2016-09-13 18:28:52 -0700

    1.1.0-rc5.1

--- Clover.js
+++ Clover.js
@@ -17,7 +17,7 @@ var Logger = require('./Logger.js');
 // !!NOTE!!  The following is automatically updated to reflect the npm version.
 // See the package.json postversion script, which maps to scripts/postversion.sh
 // Do not change this or the versioning may not reflect the npm version correctly.
-CLOVER_CLOUD_SDK_VERSION = "1.1.0-rc5.0";
+CLOVER_CLOUD_SDK_VERSION = "1.1.0-rc5.1";
 
 /**
  * Clover API for external Systems
