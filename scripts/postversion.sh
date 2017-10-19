ad73faee8a1575a7c2d7cb8f8a1ebb2d5a8727a9
mike hampton <mike@clover.com>
2017-10-19 10:05:15 -0700

    1.3.2

--- scripts/postversion.sh
+++ scripts/postversion.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo '* NPM Post version script that replaces version information'
 echo '* in library files, and in documentation.'
-echo '* This script will run after the the version of the library is changed via'
+echo '* This script should be run after the the version of the library is changed via'
 echo '* the "npm version *" command is run against this repo in preparation to publish the'
 echo '* module to npm.'
 echo "-  Replacing versioning information on files.  Version is now" $1
