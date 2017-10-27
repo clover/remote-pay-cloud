27da44820f9f1551ec147da78962c458ef0b488e
mike hampton <mike@clover.com>
2017-10-27 16:28:49 -0600

    1.4.2

--- scripts/postversion.sh
+++ scripts/postversion.sh
@@ -5,7 +5,9 @@ echo "-- Running Versioning Script --"
 #echo '* This script should be run after the the version of the library is changed via'
 #echo '* the "npm version *" command is run against this repo in preparation to publish the'
 #echo '* module to npm.'
-#echo "-  Replacing versioning information on files.  Version is now" $1
+echo "-  Attempting to replace versioning information on files.  Version is now" $1
+echo "--   Replacing versioning information on Version.ts..."
+sed -i -e 's/CLOVER_CLOUD_SDK_VERSION.*\"/CLOVER_CLOUD_SDK_VERSION = \"'$1'\"/g' src/com/clover/Version.ts
 echo "--   Replacing versioning information on README.md, attempting to change it to '$1'..."
 sed -i -e 's/Current version.*$/Current version: '$1'/g' README.md
 echo "- Committing changes to versions..."
