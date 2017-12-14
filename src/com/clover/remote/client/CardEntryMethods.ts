051f7069481d4596fe7d7950738e527c02ddbeb8
david-clover-com <david.marginian@clover.com>
2017-12-14 15:33:17 -0700

    Fix bugs found during writing the reset-test

--- src/com/clover/remote/client/CardEntryMethods.ts
+++ src/com/clover/remote/client/CardEntryMethods.ts
@@ -5,7 +5,7 @@
 export class CardEntryMethods {
     private static KIOSK_CARD_ENTRY_METHODS: number = 1 << 15;
 
-    public static CARD_ENTRY_METHOD_MAG_STRIPE: number = 0b0001 | 0b000100000000 | CardEntryMethods.KIOSK_CARD_ENTRY_METHODS; // 33026
+    public static CARD_ENTRY_METHOD_MAG_STRIPE: number = 0b0001 | 0b000100000000 | CardEntryMethods.KIOSK_CARD_ENTRY_METHODS; // 33025
     public static CARD_ENTRY_METHOD_ICC_CONTACT: number = 0b0010 | 0b001000000000 | CardEntryMethods.KIOSK_CARD_ENTRY_METHODS; // 33282
     public static CARD_ENTRY_METHOD_NFC_CONTACTLESS: number = 0b0100 | 0b010000000000 | CardEntryMethods.KIOSK_CARD_ENTRY_METHODS; // 33796
     public static CARD_ENTRY_METHOD_MANUAL: number = 0b1000 | 0b100000000000 | CardEntryMethods.KIOSK_CARD_ENTRY_METHODS; // 34824
